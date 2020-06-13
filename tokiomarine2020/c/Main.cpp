26952a0604729d59be50ace753c3c2a80038919f
yukin01 <bump1105@gmail.com>
2020-06-13 14:33:08 +0000

    imos

--- tokiomarine2020/c/Main.cpp
+++ tokiomarine2020/c/Main.cpp
@@ -9,27 +9,39 @@ int main() {
     cin >> A.at(i);
   }
 
+  if (N <= K) {
+    for (int i = 0; i < N; i++) {
+      cout << N;
+      if (i == N - 1) {
+        cout << endl;
+      } else {
+        cout << ' ';
+      }
+    }
+    return 0;
+  }
+
   vector<int> B;
   for (int i = 0; i < K; i++) {
     B = vector<int>(N, 0);
 
-    bool finish = true;
-    for (int j = 0; j < N; j ++) {
+    for (int j = 0; j < N; j++) {
       int a = A.at(j); // A_j
       int left = max(j - a, 0);
       int right = min(j + a, N - 1);
-      for (int k = left; k < right + 1; k++) {
-        B.at(k)++;
-      }
-      for (int b : B) {
-        if (b != N) {
-          finish = false;
-        }
+      // for (int k = left; k < right + 1; k++) {
+      //   B.at(k)++;
+      // }
+      B.at(left)++;
+      if (right != N - 1) {
+        B.at(right + 1)--;
       }
     }
-    A = B;
-    if (finish) {
-      break;
+    // A = B;
+    int a = 0;
+    for (int j = 0; j < N; j++) {
+      a += B.at(j);
+      A.at(j) = a;
     }
   }
 
