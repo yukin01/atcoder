20a71dd46063565da8d7529823dcc37e6675c888
yukin01 <bump1105@gmail.com>
2020-06-24 20:47:49 +0000

    virtual abc167 c

--- abc167/c/Main.cpp
+++ abc167/c/Main.cpp
@@ -14,14 +14,33 @@ int main() {
     }
   }
 
-  // rep(i, N) {
-  //   rep(j, M + 1) {
-  //     cout << books.at(i).at(j);
-  //     if (j = M) {
-  //       cout << endl;
-  //     } else {
-  //       cout << " ";
-  //     }
-  //   }
-  // }
+  // Exhaustive Search
+  int ans = -1;
+  for (int tmp = 0; tmp < (1 << N); tmp++) {
+    bitset<12> cart(tmp);  // 0: don't buy, 1: buy
+    vector<int> sum(M + 1, 0);
+
+    rep(i, N) {
+      if (cart.test(i)) continue;
+      rep(j, M + 1) {
+        sum.at(j) += books.at(i).at(j);
+      }
+    }
+
+    bool flag = true;
+    rep(j, M + 1) {
+      if (j == 0) continue;
+      if (sum.at(j) < X) {
+        flag = false;
+        break;
+      }
+    }
+    if (!flag) continue;
+
+    if (ans < 0)
+      ans = sum.at(0);
+    else
+      ans = min(ans, sum.at(0));
+  }
+  cout << ans << endl;
 }
---
+++ snippets/exhaustive_search.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+
+  bool ans = false;
+
+  // すべての選び方を試して、総和がKになるものがあるかを調べる
+  for (int tmp = 0; tmp < (1 << 20); tmp++) {
+    bitset<20> s(tmp);  // 最大20個なので20ビットのビット列として扱う
+
+    // ビット列の1のビットに対応する整数を選んだとみなして総和を求める
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+      if (s.test(i)) {
+        sum += A.at(i);
+      }
+    }
+    if (sum == K) {
+      ans = true;
+    }
+  }
+
+  if (ans) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
