774cbf8a8906478b1437f4b29e1acd358b61f223
yukin01 <bump1105@gmail.com>
2020-06-27 16:55:40 +0000

    review abc172 d

--- abc172/d/Main.cpp
+++ abc172/d/Main.cpp
@@ -4,37 +4,38 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-vector<ll> F(10000001, 0);
-
-ll prime_count(ll N, ll n, ll now) {
-  if (F.at(n) > 0) {
-    F.at(N) = F.at(n) * now;
-  }
-  for (ll a = 2; a * a <= n; a++) {
-    if (n % a != 0) continue;
-    ll ex = 0;
-    while (n % a == 0) {
-      ex++;
-      n /= a;
-    }
-    return prime_count(N, n, now * (ex + 1));
-  }
-  if (n != 1) {
-    F.at(N) = now * 2;
-    return now * 2;
-  }
-  F.at(N) = now;
-  return now;
-}
-
 int main() {
   ll N;
   cin >> N;
-
   ll ans = 0;
-  for (ll K = 1; K <= N; K++) {
-    ll fk = prime_count(K, K, 1);
-    ans += K * fk;
+
+  // NlogN + N 798 ms
+  // vector<int> F(N + 1, 0);
+  // for (ll i = 1; i <= N; i++) {
+  //   ll j = 1;
+  //   while (i * j <= N) {
+  //     F.at(i * j)++;
+  //     j++;
+  //   }
+  // }
+  // for (ll i = 1; i <= N; i++) {
+  //   ans += i * F.at(i);
+  // }
+
+  // NlogN 69 ms
+  // for (ll i = 1; i <= N; i++) {
+  //   ll j = 1;
+  //   while (i * j <= N) {
+  //     ans += i * j;
+  //     j++;
+  //   }
+  // }
+
+  // N 96 ms
+  for (ll i = 1; i <= N; i++) {
+    ll n = (N / i);
+    ans += i * n * (n + 1) / 2;
   }
+
   cout << ans << endl;
 }
