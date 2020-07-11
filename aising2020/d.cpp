d81e00021480cac68f500624a0af1e209992dcec
yukin01 <bump1105@gmail.com>
2020-07-11 17:08:17 +0000

    review aising2020 d

--- aising2020/d.cpp
+++ aising2020/d.cpp
@@ -4,39 +4,53 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-#include <boost/multiprecision/cpp_int.hpp>
-
-using namespace std;
-using Bint = boost::multiprecision::cpp_int;
-namespace mp = boost::multiprecision;
-
 int main() {
   int N;
   string X;
   cin >> N >> X;
   bitset<200000> x(X);
-  rep(i, N) {
-    bitset n = x;
-    n.flip(N - 1 - i);
 
-    if (n == 0) {
+  int popcount = x.count();
+  int hp = popcount + 1;
+  int lp = max((int)(popcount - 1), 1);  // RE 対策
+  int hr = 0;
+  int lr = 0;
+  vector<int> H(N, (1 % hp));  // pow(2,i) を hp で割った余り
+  vector<int> L(N, (1 % lp));
+  rep(i, N) {
+    if (i != 0) {
+      H.at(i) = H.at(i - 1) * 2 % hp;
+      L.at(i) = L.at(i - 1) * 2 % lp;
+    }
+    if (!x.test(i)) continue;
+    hr += H.at(i);
+    hr %= hp;
+    lr += L.at(i);
+    lr %= lp;
+  }
+
+  rep(i, N) {
+    int p = popcount + (x.test(N - 1 - i) ? -1 : 1);
+    if (p == 0) {
       cout << 0 << endl;
       continue;
     }
 
-    Bint p(n.count());
-    Bint tmp = 0;
-    rep(i, N) {
-      if (!n.test(i)) continue;
-      tmp += mp::pow(Bint(2), i) % p;
-      tmp %= p;
+    int r = 0;
+    if (p == hp) {
+      r = hr;
+      r += H.at(N - 1 - i);
+    } else if (p == lp) {
+      r = lr;
+      r -= L.at(N - 1 - i);
+      if (r < 0) r += p;
     }
+    r %= p;
 
-    n = bitset<200000>((int)tmp);
-    ll ans = 1;
-    while (n != 0) {
-      ll r = n.to_ullong() % n.count();
-      n = bitset<200000>(r);
+    int ans = 1;
+    while (r != 0) {
+      r %= bitset<20>(r).count();
+      // r %= __builtin_popcount(r);
       ans++;
     }
     cout << ans << endl;
