40946912a5ddd25b082b09343f5f5e06e5582192
yukin01 <bump1105@gmail.com>
2020-08-02 15:43:18 +0000

    abc174 d review

--- abc174/d.cpp
+++ abc174/d.cpp
@@ -7,51 +7,16 @@ using Tuple = tuple<int, int, int, string>;
 
 int main() {
   int N;
-  cin >> N;
   string C;
-  cin >> C;
+  cin >> N >> C;
   int cnt = 0;
   rep(i, N) {
     if (C.at(i) == 'R') cnt++;
   }
 
-  // priority_queue<Tuple, vector<Tuple>, greater<Tuple>> que;
-  queue<Tuple> que;
-  que.push(Tuple(0, cnt, C.size(), C));
-
-  int ans;
-  while (true) {
-    auto t = que.front();
-    que.pop();
-
-    int t1 = get<0>(t);
-    int t2 = get<1>(t);
-    int t3 = get<2>(t);
-    string t4 = get<3>(t);
-
-    if (t2 == 0 || t2 == t3) {
-      ans = t1;
-      break;
-    }
-
-    bool isR = t4.at(0) == 'R';
-    t4 = t4.substr(1);
-    t3--;
-
-    if (isR) {
-      que.push(Tuple(t1, t2 - 1, t3, t4));
-      continue;
-    }
-
-    // que.push(Tuple(t1 + 1, t2, t3, t4));
-    for (int i = t3; i > 0; i--) {
-      if (t4.at(i - 1) == 'R') {
-        string newT4 = t4;
-        newT4.at(i - 1) = 'W';
-        que.push(Tuple(t1 + 1, t2 - 1, t3, newT4));
-        break;
-      }
-    }
+  int ans = 0;
+  for (int i = cnt; i < N; i++) {
+    if (C.at(i) == 'R') ans++;
   }
 
   cout << ans << endl;
