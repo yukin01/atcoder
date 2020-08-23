0d77117a29897d6e0e5b8089b82f33cfdc2ada8f
yukin01 <bump1105@gmail.com>
2020-08-23 18:12:22 +0000

    review abc176 d

--- abc176/d.cpp
+++ abc176/d.cpp
@@ -3,53 +3,38 @@
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
+using T = tuple<int, int, int>;
 
 int main() {
   int H, W, ch, dh, cw, dw;
   cin >> H >> W >> ch >> cw >> dh >> dw;
-  vector<string> S(H);
-  rep(i, H) cin >> S.at(i);
-
-  queue<tuple<int, int, int>> que;
-
-  que.push(make_tuple(ch - 1, cw - 1, 0));
-
-  int i, j, cost;
-  while (!que.empty()) {
-    auto tp = que.front();
-    tie(i, j, cost) = tp;
-    que.pop();
-
-    queue<P> paint, find;
-    if (S.at(i).at(j) == '.') paint.push(P(i, j));
-
-    while (!paint.empty()) {
-      auto p = paint.front();
-      paint.pop();
-      find.push(p);
-      if (S.at(p.first).at(p.second) == '.')
-        S.at(p.first).at(p.second) = cost + '0';
-      if (p.first < H - 1 && S.at(p.first + 1).at(p.second) == '.')
-        paint.push(P(p.first + 1, p.second));
-      if (p.second < W - 1 && S.at(p.first).at(p.second + 1) == '.')
-        paint.push(P(p.first, p.second + 1));
-      if (p.first > 0 && S.at(p.first - 1).at(p.second) == '.')
-        paint.push(P(p.first - 1, p.second));
-      if (p.second > 0 && S.at(p.first).at(p.second - 1) == '.')
-        paint.push(P(p.first, p.second - 1));
-    }
-
-    while (!find.empty()) {
-      auto p = find.front();
-      find.pop();
-      for (int k = max(0, p.first - 2); k <= min(H - 1, p.first + 2); k++)
-        for (int l = max(0, p.second - 2); l <= min(W - 1, p.second + 2); l++)
-          if (S.at(k).at(l) == '.') que.push(make_tuple(k, l, cost + 1));
-    }
+  vector<vector<int>> S(H, vector<int>(W));
+  char c;
+  rep(i, H) rep(j, W) {
+    cin >> c;
+    S.at(i).at(j) = c == '.' ? -1 : -2;
   }
 
-  int ans = -1;
-  char goal = S.at(dh - 1).at(dw - 1);
-  if (goal != '.') ans = goal - '0';
-  cout << ans << endl;
+  deque<T> dq;
+  dq.push_front(T(ch - 1, cw - 1, 0));
+
+  int i, j, cost;
+  while (!dq.empty()) {
+    auto tp = dq.front();
+    tie(i, j, cost) = tp;
+    dq.pop_front();
+    if (S.at(i).at(j) != -1) continue;
+
+    S.at(i).at(j) = cost;
+    if (i < H - 1 && S.at(i + 1).at(j) == -1) dq.push_front(T(i + 1, j, cost));
+    if (j < W - 1 && S.at(i).at(j + 1) == -1) dq.push_front(T(i, j + 1, cost));
+    if (i > 0 && S.at(i - 1).at(j) == -1) dq.push_front(T(i - 1, j, cost));
+    if (j > 0 && S.at(i).at(j - 1) == -1) dq.push_front(T(i, j - 1, cost));
+
+    for (int k = max(0, i - 2); k <= min(H - 1, i + 2); k++)
+      for (int l = max(0, j - 2); l <= min(W - 1, j + 2); l++)
+        if (S.at(k).at(l) == -1) dq.push_back(T(k, l, cost + 1));
+  }
+
+  cout << S.at(dh - 1).at(dw - 1) << endl;
 }
