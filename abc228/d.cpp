fa4f42cc862dc82f956c449766cffb37a3b3470b
yukin01 <38382781+yukin01@users.noreply.github.com>
2021-11-20 14:20:47 +0000

    review abc228

--- abc228/d.cpp
+++ abc228/d.cpp
@@ -11,13 +11,17 @@ int main() {
 
   vector<ll> A(N, -1);
 
-  set<ll> S;
+  set<int> S;
   rep(i, N) S.insert(i);
 
   rep(i, Q) {
     cin >> t >> x;
     if (t == 1) {
-      h = *S.lower_bound(x % N);
+      auto itr = S.lower_bound(x % N);
+
+      // ヒットしなかった場合は mod 計算により 0 からやり直す
+      if (itr == S.end()) itr = S.lower_bound(0);
+      h = *itr;
 
       A.at(h) = x;
       S.erase(h);
